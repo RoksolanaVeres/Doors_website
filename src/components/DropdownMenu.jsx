be1185ff57ec2a8ff8e83ba1e4917fff1adc6d50
Roksolana Veres <Roxolanka1@ukr.net>
2024-05-27 21:44:19 +0300

    refactor: NavigationLinkItems

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -11,9 +11,13 @@ export default function DropdownMenu() {
       id="dropdown-menu"
       className="absolute bottom-0 left-0 top-0 z-10 w-2/3 bg-background px-container-padding py-20"
       onClick={closeMenuIfLinkClicked}
-      initial={{ x: "-100%" }}
-      animate={{ x: 0 }}
-      exit={{ x: "-100%" }}
+      variants={{
+        hidden: { x: "-100%" },
+        visible: { x: 0 },
+      }}
+      initial= "hidden"
+      animate="visible"
+      exit="hidden"
       transition={{ ease: "easeOut", duration: 0.5 }}
     >
       <ul className="grid gap-8">
--- src/components/NavigationLinkItems.jsx
+++ src/components/NavigationLinkItems.jsx
@@ -1,68 +1,34 @@
 import { NavLink } from "react-router-dom";
 
+
+
 export default function NavigationLinkItems() {
+  const links = [
+    { page: "Головна", path: "/" },
+    { page: "Двері", path: "/doors" },
+    { page: "Вікна", path: "/windows" },
+    { page: "Послуги", path: "/services" },
+    { page: "Контакти", path: "/contacts" },
+  ];
+
   return (
     <>
-      <li>
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? "font-semibold text-accent-main hover:underline"
-              : "font-semibold text-muted-foreground hover:underline"
-          }
-        >
-          Головна
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/doors"
-          className={({ isActive }) =>
-            isActive
-              ? "font-semibold text-accent-main hover:underline"
-              : "font-semibold text-muted-foreground hover:underline"
-          }
-        >
-          Двері
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/windows"
-          className={({ isActive }) =>
-            isActive
-              ? "font-semibold text-accent-main hover:underline"
-              : "font-semibold text-muted-foreground hover:underline"
-          }
-        >
-          Вікна
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/services"
-          className={({ isActive }) =>
-            isActive
-              ? "font-semibold text-accent-main hover:underline"
-              : "font-semibold text-muted-foreground hover:underline"
-          }
-        >
-       Послуги
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/contacts"
-          className={({ isActive }) =>
-            isActive
-              ? "font-semibold text-accent-main hover:underline"
-              : "font-semibold text-muted-foreground hover:underline"
-          }
-        >
-          Контакти
-        </NavLink>
-      </li>
+      {links.map((link)=>{
+        return (
+          <li key={link.page}>
+            <NavLink
+              to={link.path}
+              className={({ isActive }) =>
+                isActive
+                  ? "font-semibold text-accent-main hover:underline"
+                  : "font-semibold text-muted-foreground hover:underline"
+              }
+            >
+              {link.page}
+            </NavLink>
+          </li>
+        );
+      })}
     </>
   );
 }
