38e9765305e30a612e77b3cb19c8980dd317b3c5
Argyris <arkatsy@gmail.com>
2024-06-12 21:57:13 +0300

    chore: add bundle analyzer

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import {visualizer} from "rollup-plugin-visualizer";
 
-export default defineConfig(({mode}) => ({
-  plugins: [react()],
+const shouldAnalyze = process.env.ANALYZE === "true";
+
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    shouldAnalyze &&
+      visualizer({
+        template: "treemap",
+        open: true,
+        gzipSize: true,
+        filename: "dist/stats.html",
+      }),
+  ],
   esbuild: {
     pure: mode === "production" ? ["console.log"] : [],
   },
